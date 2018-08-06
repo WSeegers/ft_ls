139ab4d495cc0f7ae5a37b42328705d3b74e1956
Werner SEEGERS <wseegers@c1r2s2.wethinkcode.co.za>
2018-08-06 17:36:08 +0200

    changes to libwtc

--- bin/ft_ls.d
+++
@@ -1,5 +0,0 @@
-bin/ft_ls.o: src/ft_ls.c src/ft_ls.h libwtcc/include/f_math.h \
-  libwtcc/include/f_print.h libwtcc/include/f_io.h \
-  libwtcc/include/s_file.h libwtcc/include/f_string.h \
-  libwtcc/include/s_list.h libwtcc/include/s_list_node.h \
-  libwtcc/include/f_cntl.h
